a4380c17d427188b032d2d6ab17d1b3dc3b8eb16
Lorenzo Mori <lor.mori@gmail.com>
2015-05-31 19:47:42 +0100

    first commit

---
+++ SimpleWebScraper/SimpleWebScraper.py
@@ -0,0 +1,13 @@
+import sys
+
+def open_website(site_name):
+    print("Opening", site_name)
+    return
+
+def main():
+    print('Hello World')
+    open_website("htpp://www.kickstarter.com")
+
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+    main()
